bcd0b339f8475a8b992ca02e35ca8806ea2e8822
Joost Yervante Damad <joost@damad.be>
2017-09-27 10:30:41 +0200

    extra documentation for iteratom

--- src/iteratom.rs
+++ src/iteratom.rs
@@ -61,6 +61,7 @@ impl<'a> IterAtom<'a> {
         })
     }
 
+    /// expect a named field, retrieved by `get`
     fn expect<T, F>(&mut self, name: &str, get: F) -> SResult<T>
     where
         F: Fn(&Sexp) -> SResult<T>,
@@ -72,47 +73,65 @@ impl<'a> IterAtom<'a> {
     }
 
     /// expect an integer while iterating a `Sexp` list
+    ///
+    /// shape: (... ... 42 ...)
     pub fn i(&mut self, name: &str) -> SResult<i64> {
         self.expect(name, |x| x.i().map_err(From::from))
     }
 
     /// expect a float while iterating a `Sexp` list
+    ///
+    /// shape: (... ... 42.7 ...)
     pub fn f(&mut self, name: &str) -> SResult<f64> {
         self.expect(name, |x| x.f().map_err(From::from))
     }
 
     /// expect a String while iterating a `Sexp` list
+    ///
+    /// shape: (... ... hello ...)
     pub fn s(&mut self, name: &str) -> SResult<String> {
         self.expect(name, |x| x.s().map_err(From::from))
     }
 
     /// expect a list contained String while iterating a `Sexp` list
+    ///
+    /// shape: (... ... hello ...)
     pub fn s_in_list(&mut self, name: &str) -> SResult<String> {
         self.expect(name, |x| x.named_value_s(name).map_err(From::from))
     }
 
     /// expect a list contained i64 while iterating a `Sexp` list
+    ///
+    /// shape: (... ... (name 42) ...)
     pub fn i_in_list(&mut self, name: &str) -> SResult<i64> {
         self.expect(name, |x| x.named_value_i(name).map_err(From::from))
     }
 
     /// expect a list contained f64 while iterating a `Sexp` list
+    ///
+    /// shape: (... ... (name 42.7) ...)
     pub fn f_in_list(&mut self, name: &str) -> SResult<f64> {
         self.expect(name, |x| x.named_value_f(name).map_err(From::from))
     }
 
 
     /// expect a `Sexp` while iterating a `Sexp` list
+    ///
+    /// shape: (... ... (...) ...)
     pub fn t<T: FromSexp>(&mut self, name: &str) -> SResult<T> {
         self.expect(name, |x| T::from_sexp(x))
     }
 
     /// expect a list containing a `Sexp` while iterating a `Sexp` list
+    ///
+    /// shape: (... ... (name (...)) ...)
     pub fn t_in_list<T: FromSexp>(&mut self, name: &str) -> SResult<T> {
         self.expect(name, |x| T::from_sexp(x.named_value(name)?))
     }
 
     /// expect remainder of iterator to be a `Vec<T>`
+    ///
+    /// shape: (... T T T)
     pub fn vec<T: FromSexp>(&mut self) -> SResult<Vec<T>> {
         let mut res = Vec::new();
         loop {
@@ -128,6 +147,7 @@ impl<'a> IterAtom<'a> {
     }
 
     /// maybe something while iterating a `Sexp` list
+    /// returns `None` when the `convert` function fails
     fn maybe<X, F>(&mut self, convert: F) -> Option<X>
     where
         F: Fn(&Sexp) -> SResult<X>,
@@ -149,16 +169,22 @@ impl<'a> IterAtom<'a> {
     }
 
     /// maybe a `FromSexp` while iterating a `Sexp` list
+    ///
+    /// shape: (... ... (...) ...)
     pub fn maybe_t<T: FromSexp>(&mut self) -> Option<T> {
         self.maybe(|x| T::from_sexp(x))
     }
 
     /// maybe a `String` while iterating a `Sexp` list
+    ///
+    /// shape: (... ... hello ...)
     pub fn maybe_s(&mut self) -> Option<String> {
         self.maybe(|x| x.s())
     }
 
     /// maybe a `String` while iterating a `Sexp` list
+    ///
+    /// shape: (... ... literal ...)
     pub fn maybe_literal_s(&mut self, l: &str) -> Option<String> {
         self.maybe(|x| {
             let z = x.s()?;
@@ -171,31 +197,43 @@ impl<'a> IterAtom<'a> {
     }
 
     /// maybe an `i64` while iterating a `Sexp` list
+    ///
+    /// shape: (... ... 42 ...)
     pub fn maybe_i(&mut self) -> Option<i64> {
         self.maybe(|x| x.i())
     }
 
     /// maybe an `f64` while iterating a `Sexp` list
+    ///
+    /// shape: (... ... 42.7 ...)
     pub fn maybe_f(&mut self) -> Option<f64> {
         self.maybe(|x| x.f())
     }
 
     /// maybe a list containing a `String` while iterating a `Sexp` list
+    ///
+    /// shape: (... ... (name hello) ...)
     pub fn maybe_s_in_list(&mut self, name: &str) -> Option<String> {
         self.maybe(|x| x.named_value_s(name))
     }
 
     /// maybe a list containing an `i64` while iterating a `Sexp` list
+    ///
+    /// shape: (... ... (name 42) ...)
     pub fn maybe_i_in_list(&mut self, name: &str) -> Option<i64> {
         self.maybe(|x| x.named_value_i(name))
     }
 
     /// maybe a list containing an `f64` while iterating a `Sexp` list
+    ///
+    /// shape: (... ... (name 42.7) ...)
     pub fn maybe_f_in_list(&mut self, name: &str) -> Option<f64> {
         self.maybe(|x| x.named_value_f(name))
     }
 
     /// make sure we consumed all of the iterator
+    ///
+    /// shape: )
     pub fn close<T>(&mut self, x: T) -> SResult<T> {
         match self.iter.next() {
             Some(x) => Err(format!("Unexpected {} in {}", x, self.name).into()),
